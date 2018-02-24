9aac5ae9eb13722ab2d36483ce03da67267f034a
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-02-24 23:37:28 +0000

    More functions added

--- src/java/pack/calc.java
+++ src/java/pack/calc.java
@@ -1,5 +1,4 @@
 package pack;
-
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -8,9 +7,11 @@ import javax.ejb.Stateless;
 @WebService(serviceName = "calc")
 @Stateless()
 public class calc {
-
+    
     /**
      * This is a sample web service operation
+     * @param txt
+     * @return 
      */
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
@@ -18,20 +19,52 @@ public class calc {
     }
 
     /**
-     * Web service operation
+     * Web service operation : Sum 2 numbers
+     * @param n1
+     * @param n2
+     * @return 
      */
+    
     @WebMethod(operationName = "Sum")
     public int Sum(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
-       int total = n1 + n2; 
-        return total;
+        return (n1+n2);
     }
 
     /**
-     * Web service operation
+     * Web service operation : Subtract 2 numbers
+     * @param n1
+     * @param n2
+     * @return 
      */
+    
     @WebMethod(operationName = "rest")
     public int rest(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
-        int total = n1 - n2;
-        return total;
+        return (n1-n2);
+    }
+
+    /**
+     * Web service operation : Multiply 2 numbers
+     * @param n1
+     * @param n2
+     * @return 
+     */
+    
+    @WebMethod(operationName = "mult")
+    public int mult(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
+        return (n1*n2);
+    }
+
+    /**
+     * Web service operation : Divide 2 numbers if divisor is not 0
+     * If divisor is 0, return -1
+     * @param n1
+     * @param n2
+     * @return 
+     */
+    
+    @WebMethod(operationName = "div")
+    public float div(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
+        if (n2 != 0) return (float)n1/n2;
+        return -1;
     }
 }
